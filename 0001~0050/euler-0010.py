7458f4e6b4edec95de993ecc0b9617ef7f61600c
m-star18 <ryuuchei@ezweb.ne.jp>
2020-05-18 19:49:58 +0900

    solving 0010

--- 0001~0050/euler-0010.py
+++ 0001~0050/euler-0010.py
@@ -1,13 +1,13 @@
-def makePrimeChecker(n):
-    isPrime = [True] * (n + 1)
-    isPrime[0] = False
-    isPrime[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
-        if isPrime[i]:
-            for j in range(i * i, n + 1, i):
-                isPrime[j] = False
-    return isPrime
+def eratosthenes(n):
+    prime_list = [True] * (n + 1)
+    prime_list[0] = False
+    prime_list[1] = False
+    for i, prime in enumerate(prime_list[2:int(n ** 0.5) + 1]):
+        if prime:
+            for j in range(pow(i + 2, 2), n + 1, i + 2):
+                prime_list[j] = False
+    return prime_list
 
 
 limit = 2 * 10 ** 6
-print(sum(i if v else 0 for i, v in enumerate(makePrimeChecker(limit))))
+print(sum(i if v else 0 for i, v in enumerate(eratosthenes(limit))))
