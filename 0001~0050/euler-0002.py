bab70a420f5b7a20a37669a0d6212a3f9201aa3c
m-star18 <ryuuchei@ezweb.ne.jp>
2020-05-18 16:33:47 +0900

    solving 0003

--- 0001~0050/euler-0002.py
+++ 0001~0050/euler-0002.py
@@ -1,6 +1,6 @@
-fibonacci = [1, 2]
+fib = [1, 2]
 ans = 2
-while fibonacci[-1] + fibonacci[-2] < 4 * 10 ** 6:
-    fibonacci.append(fibonacci[-1] + fibonacci[-2])
-    ans += fibonacci[-1] if fibonacci[-1] % 2 == 0 else 0
+while fib[-1] + fib[-2] < 4 * 10 ** 6:
+    fib.append(fib[-1] + fib[-2])
+    ans += fib[-1] if fib[-1] % 2 == 0 else 0
 print(ans)
