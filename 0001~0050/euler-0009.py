c1c5cf4369a1db8970600ce5bfd6fd4e258688b3
m-star18 <ryuuchei@ezweb.ne.jp>
2020-05-18 17:08:41 +0900

    solving 0009

---
+++ 0001~0050/euler-0009.py
@@ -0,0 +1,8 @@
+from math import sqrt
+
+for a in range(0, 1000):
+    for b in range(a + 1, 1000):
+        c = sqrt(a ** 2 + b ** 2)
+        if a + b + c == 1000 and b < c:
+            print(a, b, c)
+            exit()
