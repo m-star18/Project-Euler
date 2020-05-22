81f06e6b90b5d9d409c8addc908fe1d0c85bb1f8
m-star18 <ryuuchei@ezweb.ne.jp>
2020-05-22 22:55:16 +0900

    solving 0021

--- 0001~0050/0021~0030/euler-0021.py
+++ 0001~0050/0021~0030/euler-0021.py
@@ -1,18 +1,17 @@
 def make_divisors(n):
-    divisors = []
+    res = 0
     for i in range(1, int(n ** 0.5) + 1):
         if n % i == 0:
-            divisors.append(i)
+            res += i
             if i != n // i:
-                divisors.append(n // i)
+                res += n // i
 
-    # divisors.sort()
-    return divisors
+    return res
 
 
 ans = 0
 for a in range(1, 10 ** 4):
-    b = sum(make_divisors(a)) - a
-    if a + b == sum(make_divisors(b)) and a != b:
+    b = make_divisors(a) - a
+    if a + b == make_divisors(b) and a != b:
         ans += a
 print(ans)
