9c024a3580a881d41017cf0c9189f7e297bff3eb
m-star18 <ryuuchei@ezweb.ne.jp>
2020-05-24 15:38:26 +0900

    solving 0036

--- 0001~0050/0031~0040/euler-0036.py
+++ 0001~0050/0031~0040/euler-0036.py
@@ -1,14 +1,9 @@
-limit = 10 ** 6
-ans = 0
-for i in range(limit):
-    for bf, af in zip(str(i), str(i)[::-1]):
+def check(n):
+    for bf, af in zip(n, n[::-1]):
         if bf != af:
-            break
-    else:
-        j = bin(i)[2:]
-        for bf, af in zip(j, j[::-1]):
-            if bf != af:
-                break
-        else:
-            ans += i
-print(ans)
+            return False
+    return True
+
+
+limit = 10 ** 6
+print(sum(i if check(str(i)) and check(bin(i)[2:]) else 0 for i in range(limit)))
