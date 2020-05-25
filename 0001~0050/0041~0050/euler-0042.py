b9210b14f351107d50a12c412e3457172384dceb
m-star18 <ryuuchei@ezweb.ne.jp>
2020-05-26 01:04:42 +0900

    solving 0042

--- 0001~0050/0041~0050/euler-0042.py
+++ 0001~0050/0041~0050/euler-0042.py
@@ -3,8 +3,4 @@ from itertools import accumulate
 cumsum = list(accumulate(list(range(100))))
 al = {chr(m): i + 1 for i, m in enumerate(range(65, 91))}
 data = open('words.txt', 'r').readline().split(sep=',')
-cnt = 0
-for i, d in enumerate(data):
-    if sum(al[w] for w in d.replace('"', '')) in cumsum:
-        cnt += 1
-print(cnt)
+print(sum(1 if sum(al[w] for w in d.replace('"', '')) in cumsum else 0 for d in data))
