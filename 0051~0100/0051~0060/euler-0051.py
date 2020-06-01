d50933059c8ecae936ad12710b82438f59b73830
m-star18 <ryuuchei@ezweb.ne.jp>
2020-06-01 16:14:01 +0900

    solving 0051

--- 0051~0100/0051~0060/euler-0051.py
+++ 0051~0100/0051~0060/euler-0051.py
@@ -11,21 +11,21 @@ def eratosthenes(n):
 
 limit = 10 ** 6
 prime_check = eratosthenes(limit)
+end = 10
 for num in range(56004, limit):
     if not prime_check[num]:
         continue
     num = str(num)
-    for check in range(10):
+    for check in range(end):
         check = str(check)
         m = ''
         for n in num:
             m += (n if n != check else '?')
         if '?' not in m:
             continue
+        start = (1 if m[0] == '?' else 0)
         cnt = 0
-        for i in range(10):
-            if i == 0 and m[0] == '?':
-                continue
+        for i in range(start, end):
             if prime_check[int(m.replace('?', str(i)))]:
                 cnt += 1
         if cnt >= 8:
