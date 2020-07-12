40829ec49fd38b3f2f1a7eb2f660e42b8a2cd4e7
m-star18 <ryuuchei@ezweb.ne.jp>
2020-07-13 01:28:35 +0900

    solving 0092

--- 0051~0100/0091~0100/euler-0092.py
+++ 0051~0100/0091~0100/euler-0092.py
@@ -1,8 +1,14 @@
-cnt = 0
+from functools import lru_cache
+
+
+@lru_cache()
+def check(n):
+    if n == 1:
+        return False
+    if n == 89:
+        return True
+    return check(sum(int(x) ** 2 for x in str(n)))
+
+
 limit = 10 ** 7
-for i in range(1, limit):
-    while i != 1 and i != 89:
-        i = sum(int(x) ** 2 for x in str(i))
-    if i == 89:
-        cnt += 1
-print(cnt)
+print(sum(1 for i in range(1, limit) if check(i)))
